e679d3194c87c7fd8472f9ca79010a7185a6edd3
dzlek <6894255@gmail.com>
2024-08-21 13:46:00 +0300

    init: and done

--- src/index.js
+++ src/index.js
@@ -1,3 +1,8 @@
-module.exports = function reverse (n) {
-  
-}
+module.exports = function reverse(n) {
+    n = String(n < 0 ? -n : n)
+        .split("")
+        .reverse()
+        .join("");
+
+    return n;
+};
